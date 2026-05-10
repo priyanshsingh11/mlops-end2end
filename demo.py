d54be06f9b7adcfeae9b591b4d435e1b3468e96f
Priyansh Singh <priyanshsingh855@gmail.com>
2026-05-10 23:39:37 +0530

    feat: initialize project structure, add MongoDB integration notebook, and implement custom logger and exception handlers

---
+++ demo.py
@@ -0,0 +1,9 @@
+# from src.logger import logging 
+# from src.exception import MyException
+# import sys
+
+# try:
+#     logging.info("Testing the custom logger and exception...")
+#     a = 1 + 'Z' # This will trigger a TypeError
+# except Exception as e:
+#     raise MyException(e, sys) from e
