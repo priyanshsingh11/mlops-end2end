f50148ff2c9fc74fc84565bf5e8785071e9e805f
Priyansh Singh <priyanshsingh855@gmail.com>
2026-05-10 23:14:21 +0530

    feat: initialize project structure with gitignore, requirements, and setup files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Priyansh Singh",
+    author_email="priyanshsingh855@gmail.com",
+    packages=find_packages()
+)
